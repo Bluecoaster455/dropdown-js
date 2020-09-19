0888aae3c4479016ad00052c4deec755d48efffa
Bluecoaster455 <bluecoaster455@gmail.com>
2020-09-19 15:37:42 -0400

    demo actions

---
+++ demo/script.js
@@ -0,0 +1,11 @@
+document.querySelector(".action-user").addEventListener("click", function(){
+    alert("Clicked user option!")
+})
+
+document.querySelector(".action-news").addEventListener("click", function(){
+    alert("Clicked news option!")
+})
+
+document.querySelector(".action-settings").addEventListener("click", function(){
+    alert("Clicked settings option!")
+})
