bbd23f936759fc136e94b5a1e6b7559e1994681d
Bluecoaster455 <bluecoaster455@gmail.com>
2020-09-19 21:04:01 -0400

    fixing the demo

--- demo/script.js
+++ demo/script.js
@@ -9,3 +9,5 @@ document.querySelector(".action-news").addEventListener("click", function(){
 document.querySelector(".action-settings").addEventListener("click", function(){
     alert("Clicked settings option!")
 })
+
+DropdownJS.init();
